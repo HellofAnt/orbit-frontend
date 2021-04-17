804fc81eab854bd9b75a0bc99044ef02141abff0
Nill <wwaosldk@gmail.com>
2021-04-18 01:17:40 +0900

    Navi setting(ORBIT1129-T-6)

--- orbit/src/components/Navi.js
+++ orbit/src/components/Navi.js
@@ -1,43 +1,23 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles(() => ({
+  sideBar: {
+    position: "fixed",
+    width: "70px",
+    height: "100%",
+    left: "0",
+    backgroundColor: "brown",
+    zIndex: "2000",
+  },
+  marginSpace: {
+    marginLeft: "70px",
+    height: "100%",
+  },
+}));
 
-const useStyles = makeStyles((theme) => ({
-    icon: {
-      marginRight: theme.spacing(2),
-    },
-    heroContent: {
-      backgroundColor: theme.palette.background.paper,
-      padding: theme.spacing(8, 0, 6),
-    },
-    heroButtons: {
-      marginTop: theme.spacing(4),
-    },
-    cardGrid: {
-      paddingTop: theme.spacing(8),
-      paddingBottom: theme.spacing(8),
-    },
-    card: {
-      height: '100%',
-      display: 'flex',
-      flexDirection: 'column',
-    },
-    cardMedia: {
-      paddingTop: '56.25%', // 16:9
-    },
-    cardContent: {
-      flexGrow: 1,
-    },
-    footer: {
-      backgroundColor: theme.palette.background.paper,
-      padding: theme.spacing(6),
-    },
-  }));
-
-function Navi(){
-    return (
-        <div className="">
-
-        </div>
-    )
+function Navi() {
+  const classes = useStyles();
+  return <div className={classes.sideBar}>test</div>;
 }
+export default Navi;
--- orbit/src/views/Challenge.js
+++ orbit/src/views/Challenge.js
@@ -1,153 +1,181 @@
 import React from "react";
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import CameraIcon from "@material-ui/icons/PhotoCamera";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+import Navi from "../components/Navi";
 function Copyright() {
-    return (
+  return (
     <Typography variant="body2" color="textSecondary" align="center">
-        {'Copyright © '}
-        <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-        </Link>{' '}
-        {new Date().getFullYear()}
-        {'.'}
+      {"Copyright © "}
+      <Link color="inherit" href="#">
+        Orbit-Team
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
     </Typography>
-    );
+  );
 }
 
 const useStyles = makeStyles((theme) => ({
-    icon: {
+  appBar: {
+    justifyContent: "flex-end",
+  },
+  icon: {
     marginRight: theme.spacing(2),
-    },
-    heroContent: {
+  },
+  heroContent: {
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(8, 0, 6),
-    },
-    heroButtons: {
+    marginLeft: "70px",
+  },
+  heroButtons: {
     marginTop: theme.spacing(4),
-    },
-    cardGrid: {
+  },
+  cardGrid: {
     paddingTop: theme.spacing(8),
     paddingBottom: theme.spacing(8),
-    },
-    card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    },
-    cardMedia: {
-      paddingTop: '56.25%', // 16:9
-    },
-    cardContent: {
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "56.25%", // 16:9
+    filter: "grayscale(100)",
+  },
+  cardContent: {
     flexGrow: 1,
-    },
-    footer: {
+  },
+  footer: {
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(6),
-    },
+  },
 }));
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let cards = new Array();
+let cardTotalNum = 16;
+for (let i = 0; i < cardTotalNum; i++) {
+  cards.push(i);
+}
 
-function Challenge(){
-    const classes = useStyles();
+function Challenge() {
+  const classes = useStyles();
 
-    return(
-        <React.Fragment>
-        <CssBaseline />
-        <AppBar position="relative">
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Navi />
+      <AppBar className={classes.appBar} position="relative">
         <Toolbar>
-            <CameraIcon className={classes.icon} />
-            <Typography variant="h6" color="inherit" noWrap>
-            Album layout
-            </Typography>
+          <CameraIcon className={classes.icon} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Header
+          </Typography>
         </Toolbar>
-        </AppBar>
-        <main>
-          {/* Hero unit */}
+      </AppBar>
+      <main>
+        {/* Hero unit */}
         <div className={classes.heroContent}>
-            <Container maxWidth="sm">
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-                Album layout
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Album layout
             </Typography>
-            <Typography variant="h5" align="center" color="textSecondary" paragraph>
-                Something short and leading about the collection below—its contents, the creator, etc.
-                Make it short and sweet, but not too short so folks don&apos;t simply skip over it
-                entirely.
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Something short and leading about the collection below—its
+              contents, the creator, etc. Make it short and sweet, but not too
+              short so folks don&apos;t simply skip over it entirely.
             </Typography>
             <div className={classes.heroButtons}>
-                <Grid container spacing={2} justify="center">
+              <Grid container spacing={2} justify="center">
                 <Grid item>
-                    <Button variant="contained" color="primary">
+                  <Button variant="contained" color="primary">
                     Main call to action
-                    </Button>
+                  </Button>
                 </Grid>
                 <Grid item>
-                    <Button variant="outlined" color="primary">
+                  <Button variant="outlined" color="primary">
                     Secondary action
-                    </Button>
+                  </Button>
                 </Grid>
-                </Grid>
+              </Grid>
             </div>
-            </Container>
+          </Container>
         </div>
         <Container className={classes.cardGrid} maxWidth="md">
-            {/* End hero unit */}
-            <Grid container spacing={4}>
+          {/* End hero unit */}
+          <Grid container spacing={4}>
             {cards.map((card) => (
-                <Grid item key={card} xs={12} sm={6} md={4}>
+              <Grid item key={card} xs={12} sm={6} md={4}>
                 <Card className={classes.card}>
-                    <CardMedia
+                  <CardMedia
                     className={classes.cardMedia}
                     image="https://source.unsplash.com/random"
                     title="Image title"
-                    />
-                    <CardContent className={classes.cardContent}>
+                  />
+                  <CardContent className={classes.cardContent}>
                     <Typography gutterBottom variant="h5" component="h2">
-                        Heading
+                      Heading
                     </Typography>
                     <Typography>
-                        This is a media card. You can use this section to describe the content.
+                      This is a media card. You can use this section to describe
+                      the content.
                     </Typography>
-                    </CardContent>
-                    <CardActions>
+                  </CardContent>
+                  <CardActions>
                     <Button size="small" color="primary">
-                        View
+                      View
                     </Button>
                     <Button size="small" color="primary">
-                        Edit
+                      Edit
                     </Button>
-                    </CardActions>
+                  </CardActions>
                 </Card>
-            </Grid>
+              </Grid>
             ))}
-            </Grid>
+          </Grid>
         </Container>
-        </main>
-        {/* Footer */}
-        <footer className={classes.footer}>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
         <Typography variant="h6" align="center" gutterBottom>
-            Footer
+          Make your life
         </Typography>
-        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-            Something here to give the footer a purpose!
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="textSecondary"
+          component="p"
+        >
+          Something here to give the footer a purpose!
         </Typography>
         <Copyright />
-        </footer>
-        {/* End footer */}
+      </footer>
+      {/* End footer */}
     </React.Fragment>
-    )
-};
+  );
+}
 
 export default Challenge;
