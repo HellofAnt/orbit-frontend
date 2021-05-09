c38250311bea0fc6d552f05d1ccf123ecc1f6024
eunalee <luvlylunalee@gmail.com>
2021-05-09 15:13:07 +0900

    Orbit 프론트 페이지 - Routine (ORBIT1129-T-6)

--- orbit/src/App.js
+++ orbit/src/App.js
@@ -1,10 +1,13 @@
 import React from "react"
 import "./App.css";
+import Routine from "./views/Routine";
 import Challenge from "./views/Challenge";
+
 
 function App() {
   return (
     <div className="App">
+      <Routine />
       <Challenge />
     </div>
   )
